bb27e86bde97fff5bc3e200b20eec5a8754e9d4c
Ader1337 <91737396+Ader1337@users.noreply.github.com>
2022-06-05 15:32:51 +0300

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
                     <nav className="header__nav">
                         <ul className="header__nav-list">
                             <li className="header__nav-item">
-                                <a className="header__nav-link" href="#">Repository</a>
+                                <a className="header__nav-link" href="https://github.com/Ader1337/react-movies">Repository</a>
                             </li>
                         </ul>
                     </nav>
